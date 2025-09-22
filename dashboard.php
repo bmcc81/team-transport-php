8a51f7b963048dcbe51b03fead6e51f548c1610d
bmcc81 <bmcc81@gmail.com>
2025-09-22 08:39:01 -0400

    Added foreach for the table, moved the logout btn and created the GET request

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,8 @@ session_start();
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
+} else {
+    include "includes/user_dashboard_customers.php";
 }
 ?>
 <!DOCTYPE html>
@@ -12,13 +14,17 @@ if (!isset($_SESSION['user_id'])) {
 <head>
   <title>Dashboard</title>
   <link href="./styles/css/bootstrap.min.css" rel="stylesheet">
+  <link href="./styles/shared.css" rel="stylesheet">
 </head>
 <body class="p-4">
+  <div class="logout-btn">
+    <a href="views/logout.php" class="btn btn-danger">Logout</a>
+  </div>
+  <div>
   <h1><?php echo $_SESSION['username'];?>'s Client Dashboard</h1>
   <table class="table table-striped table-hover">
     <thead>
       <tr>
-        <th scope="col">User</th>
         <th scope="col">Company Name</th>
         <th scope="col">Client Owner</th>
         <th scope="col">First Name</th>
@@ -35,25 +41,32 @@ if (!isset($_SESSION['user_id'])) {
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Walmart</td>
-        <td>Brandon</td>
-        <td>Tim</td>
-        <td>Shoniker</td>
-        <td>tim@gmail.com</td>
-        <td>123 Street</td>
-        <td>Toronto</td>
-        <td>Ontario</td>
-        <td>Canada</td>
-        <td>514-555-9999</td>
-        <td>514-555-9991</td>
-        <td>www.tim.com</td>
-        <td>2025-09-21 17:44:08</td>
-      </tr>
+      <?php if (!empty($customers)): ?>
+      <?php foreach ($customers as $customer): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($customer['customer_company_name']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_internal_handler_name']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_contact_first_name']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_contact_last_name']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_email']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_contact_address']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_contact_city']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_contact_state_or_province']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_contact_country']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_phone']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_fax']); ?></td>
+            <td><?php echo htmlspecialchars($customer['customer_website']); ?></td>
+            <td><?php echo $customer['created_at']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+      <?php else: ?>
+        <tr>
+          <td colspan="14">No customers found.</td>
+        </tr>
+      <?php endif; ?>
     </tbody>
+
   </table>
   <a href="views/create_customer_view.php" class="btn btn-success">Create Customer</a>
-  <a href="views/logout.php" class="btn btn-danger">Logout</a>
 </body>
 </html>
