11cc7a6bad89d9007331a279f86b3a332a8174fd
bmcc81 <bmcc81@gmail.com>
2025-10-02 15:45:16 -0400

    FIXED SQL Database for customer handler mistake, created if statement for admin to choose who to assign a customer

--- includes/SQL/team_transport.sql
+++ includes/SQL/team_transport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2025 at 10:18 PM
+-- Generation Time: Oct 02, 2025 at 09:30 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,27 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `team_transport`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `comment_text` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT curtime(),
-  `users_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`id`, `username`, `comment_text`, `created_at`, `users_id`) VALUES
-(1, 'Brandon', 'This is a brandon comment', '2025-09-15 18:38:09', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,9 +51,13 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `user_id`, `customer_company_name`, `customer_internal_handler_name`, `customer_contact_first_name`, `customer_contact_last_name`, `customer_email`, `customer_contact_address`, `customer_contact_city`, `customer_contact_state_or_province`, `customer_contact_zip_or_postal_code`, `customer_contact_country`, `customer_phone`, `customer_fax`, `customer_website`, `created_at`) VALUES
-(2, 8, 'Walmart', 'Brandon', 'Tim', 'Shoniker', 'tim@gmail.com', '123 street', 'Toronto', 'Ontario', 'h7g5a9', 'Canada', '514-555-9999', '555-999-9991', 'www.tim.com', '2025-09-21 21:44:08'),
-(3, 8, 'Best Buy', 'Melissa', 'Tom', 'McCarthy', 'tom@gmail.com', '16 Kelloway', 'Harrington', 'Quebec', 'J7f3t6', 'Canada', '514-999-7777', '514-999-7771', 'www.tom.com', '2025-09-22 00:36:27'),
-(4, 8, 'Canadian Tire', 'Brandon', 'John', 'Doe', 'john@ct.com', '999 Lake Road', 'Ottawa', 'Ontario', 'y8ys4s', 'Canada', '416-888-9999', '416-888-9991', 'www.canadientire.ca', '2025-09-22 01:07:22');
+(2, 8, 'Walmart', 'Brandon', 'Tim', 'Shoniker', 'tim@gmail.com', '123 street', 'Toronto', 'Ontario', 'h7g5a9', 'Canada', '514-555-9999', '555-999-9991', 'www.tim.com', '2025-09-22 01:44:08'),
+(3, 8, 'Best Buy', 'Melissa', 'Tom', 'McCarthy', 'tom@gmail.com', '16 Kelloway', 'Harrington', 'Quebec', 'J7f3t6', 'Canada', '514-999-7777', '514-999-7771', 'www.tom.com', '2025-09-22 04:36:27'),
+(4, 8, 'Canadian Tire', 'Brandon', 'John', 'Doe', 'john@ct.com', '999 Lake Road', 'Ottawa', 'Ontario', 'y8ys4s', 'Canada', '416-888-9999', '416-888-9991', 'www.canadientire.ca', '2025-09-22 05:07:22'),
+(5, 11, 'Super C', 'Tom', 'Mike', 'Furlotte', 'mike@gmail.com', '46 Super-C Street', 'Vaudreuil', 'Quebec', 'h7h5at', 'Canada', '555-666-9999', '555-666-9999', 'www.supec.com', '2025-09-29 19:30:04'),
+(6, 11, 'Tim Hortons', 'Tom', 'Bob', 'Horton', 'bob_horton@timhortons.ca', '44 Tims Street', 'Edmonton', 'Alberta', '4d59i8', 'Canada', '514-833-9991', '514-833-9992', 'www.timhortons.ca', '2025-09-29 19:58:33'),
+(7, 13, 'McDonalds', 'Bob', 'Ronald', 'McDonald', 'ron@mcdonalds.com', '123 Big Mac Street', 'Burger Town', 'Quebec', 'j8h6f7', 'Canada', '555-666-9999', '555-666-9991', 'www.mcdonalds.com', '2025-10-01 12:48:34'),
+(8, 13, 'Pharmaprix', 'Bob', 'JOhn', 'Doe', 'johndoe@gmail.com', '333 DrugsStore Blvd.', 'New York', 'New York', '99802', 'United States', '555-777-8888', '555-777-8888', 'www.pharmaprix.com', '2025-10-01 12:51:01');
 
 -- --------------------------------------------------------
 
@@ -99,7 +82,10 @@ INSERT INTO `users` (`id`, `username`, `pwd`, `email`, `created_at`, `role`) VAL
 (7, 'testuser', '$2y$10$SQ1BvSmj0k/iaQmDR1CpUOJXF4bsXlfUfsbgmqj/6H66Cwh0S5WOy', 'some_email@gmail.com', '2025-09-16 12:01:46', 'user'),
 (8, 'Brandon', '$2y$10$FiBy0DEl3IFhWHmwWpwVOe32DpwwZPs6SF2vi89shnu3awKDkFYrC', 'bmcc81@gmail.com', '2025-09-16 12:02:23', 'user'),
 (9, 'Melissa', '$2y$10$plfozzDp.TmhBVTVgkxxJuBbfr44JhjDOt/Icyd3lVWX27R0/bN4u', 'melissa@gmail.com', '2025-09-16 12:02:55', 'user'),
-(10, 'admin', '$2y$10$XY0c05j4qtNQbhgKrq02iOnu3.9.Sk1BCky/WhJmQvWJ76TVp38VG', 'bmcc81@gmail.com', '2025-09-18 16:00:22', 'admin');
+(10, 'admin', '$2y$10$XY0c05j4qtNQbhgKrq02iOnu3.9.Sk1BCky/WhJmQvWJ76TVp38VG', 'bmcc81@gmail.com', '2025-09-18 16:00:22', 'admin'),
+(11, 'Tom', '$2y$10$X89422LRfjCFYMHaXE4Nr.TIJto4NC60.WV2/HMX3.Jx/ZMvi9esS', 'tom@gmail.com', '2025-09-29 12:01:51', 'user'),
+(13, 'Bob', '$2y$10$vHkFzQZoV6qbJRJ/9XEry.p.cIj13vgbjNJD1cQ7LDktmcpdDAIea', 'bob@gmail.com', '2025-09-30 14:16:27', 'user'),
+(14, 'Charlie', '$2y$10$OSZUk/PkoWLUpy3ENSX64OcPFM.v8wb3rXD1hoOUMd.1ou.fVdFXa', 'charlie@gmail.com', '2025-09-30 15:36:56', 'user');
 
 --
 -- Triggers `users`
@@ -119,13 +105,6 @@ DELIMITER ;
 --
 
 --
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `users_id` (`users_id`);
-
---
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
@@ -143,32 +122,20 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
 
 --
 -- Constraints for table `customers`
