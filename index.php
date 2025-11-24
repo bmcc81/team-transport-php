81457c73b49250f4c74c154969689a401554d264
Brandon McCarthy <bmcc81@gmail.com>
2025-11-24 09:34:48 -0500

    Updated the css and created load_views.css, movbed some divs for styling an difxed the font(s) folder name, which became broken (#42)

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
 <head>
   <meta charset="UTF-8">
   <title>Team Transport - Login</title>
-  <link href="./styles/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
-  <link rel="manifest" href="./site.webmanifest">
+    <link href="./styles/css/bootstrap.min.css" rel="stylesheet">
+    <script src="./styles/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Manifest -->
+    <link rel="manifest" href="./site.webmanifest">
+
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/icons/favicon-16x16.png">
 </head>
 <body class="bg-light">
 
